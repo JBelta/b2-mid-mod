efcc8bdff1a84e259bb8bdbb33c98cfa7399ad09
James Belta <james.belta@gmail.com>
2020-10-23 10:01:17 -0600

    change directory name

--- db/migrate/20201023152234_create_amusment_parks.rb
+++
@@ -1,8 +0,0 @@
-class CreateAmusmentParks < ActiveRecord::Migration[5.2]
-  def change
-    create_table :amusment_parks do |t|
-      t.string :park_name
-      t.integer :price
-    end
-  end
-end
--- db/migrate/20201023152732_add_amusment_parks_to_rides.rb
+++
@@ -1,5 +0,0 @@
-class AddAmusmentParksToRides < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :rides, :amusment_park, foreign_key: true
-  end
-end
