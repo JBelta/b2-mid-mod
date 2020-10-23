1532c152aa96c1008d634aefee18c4be59275ccf
James Belta <james.belta@gmail.com>
2020-10-23 11:54:00 -0600

    creat action needs refactorind

--- app/controllers/ride_mechanics_controller.rb
+++ app/controllers/ride_mechanics_controller.rb
@@ -1,9 +1,10 @@
 class RideMechanicsController < ApplicationController
 
   def create
-    binding.pry
     @ride = Ride.where(ride_name: params["Ride"])
-    @mechanic = Mechanic.where(id: params[:mechanic_id])
-    RideMechanic.create(ride_id: @ride.id, mechanic_id: @mechanic.id)
+    @mechanic = Mechanic.find(params[:mechanic_id])
+    binding.pry
+    RideMechanic.create(ride_id: @ride.first.id, mechanic_id: @mechanic.id)
+    redirect_to "/mechanics/#{@mechanic.id}"
   end
 end
