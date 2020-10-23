cc941e82c7417c468b02c6988fb7931ba25d83cc
James Belta <james.belta@gmail.com>
2020-10-23 09:54:37 -0600

    changed name: to park_name:

--- spec/features/amusment_parks/show_spec.rb
+++ spec/features/amusment_parks/show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "As a user" do
   describe "When I visit an amusment parks show page" do
     it "Has the name and price of admission for that park"
-    dz = AmusmentPark.create(name: "Discovery Zone", price: 13)
+    dz = AmusementPark.create(park_name: "Discovery Zone", price: 13)
 
     visit "/amusment_parks/#{dz.id}"
 
