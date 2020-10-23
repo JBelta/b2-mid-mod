0f235d35d02f711e0f84d25ab1a547a58bdbf7a3
James Belta <james.belta@gmail.com>
2020-10-23 10:15:49 -0600

    Added to spec ride names and average thrill rating

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -10,5 +10,18 @@ describe "As a user" do
       expect(page).to have_content(dz.park_name)
       expect(page).to have_content(dz.price)
     end
+
+    it "Has a list of all rides with an average thrill rating"
+    dz = AmusementPark.create(park_name: "Discovery Zone", price: 13)
+
+    the_rocket = dz.rides.create(ride_name: "The Rocket", thrill_rating: 10)
+
+    go_round = dz.rides.create(ride_name: "Merry Go Round", thrill_rating: 3)
+
+    visit "/amusement_parks/#{dz.id}"
+
+    expect(page).to have_content(the_rocket.ride_name)
+    expect(page).to have_content(go_round.ride_name)
+    expect(page).to have_content(6.5)
   end
 end
