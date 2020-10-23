95cd53348134009c3e5241478980e4874d7941f1
James Belta <james.belta@gmail.com>
2020-10-23 10:03:53 -0600

    spelling

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -5,7 +5,7 @@ describe "As a user" do
     it "Has the name and price of admission for that park" do
       dz = AmusementPark.create(park_name: "Discovery Zone", price: 13)
 
-      visit "/amusment_parks/#{dz.id}"
+      visit "/amusement_parks/#{dz.id}"
 
       expect(page).to have_content(dz.name)
       expect(page).to have_content(dz.price)
