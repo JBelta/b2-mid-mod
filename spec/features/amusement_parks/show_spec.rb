e137badabe6008c5b0319b17fd6b728a13f49cef
James Belta <james.belta@gmail.com>
2020-10-23 10:02:14 -0600

    do / end after it ""

---
+++ spec/features/amusement_parks/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "As a user" do
+  describe "When I visit an amusment parks show page" do
+    it "Has the name and price of admission for that park" do
+      dz = AmusementPark.create(park_name: "Discovery Zone", price: 13)
+
+      visit "/amusment_parks/#{dz.id}"
+
+      expect(page).to have_content(dz.name)
+      expect(page).to have_content(dz.price)
+    end
+  end
+end
